7230218f53c259a31fd586c18cc4ced4eb201b6d
Patrick Graham <patrickgraham128@gmail.com>
2024-05-26 19:56:50 +1000

    placeholder for NULL odds

--- free-bet/src/components/PlayerCard.js
+++ free-bet/src/components/PlayerCard.js
@@ -8,12 +8,12 @@ export const PlayerCard = ({playerOdds}) => {
   return (
     <div className='player-card-container'>
       <div className='player-name'>{playerName}</div>
-      <button className='bet-button'>{playerOdds[2]}</button>
-      <button className='bet-button'>{playerOdds[3]}</button>
-      <button className='bet-button'>{playerOdds[4]}</button>
-      <button className='bet-button'>{playerOdds[5]}</button>
-      <button className='bet-button'>{playerOdds[6]}</button>
-      <button className='bet-button'>{playerOdds[7]}</button>
+      <button className='bet-button'>{playerOdds[2] == 'NULL' ? 'N/A' : playerOdds[2]}</button>
+      <button className='bet-button'>{playerOdds[3] == 'NULL' ? 'N/A' : playerOdds[3]}</button>
+      <button className='bet-button'>{playerOdds[4] == 'NULL' ? 'N/A' : playerOdds[4]}</button>
+      <button className='bet-button'>{playerOdds[5] == 'NULL' ? 'N/A' : playerOdds[5]}</button>
+      <button className='bet-button'>{playerOdds[6] == 'NULL' ? 'N/A' : playerOdds[6]}</button>
+      <button className='bet-button'>{playerOdds[7] == 'NULL' ? 'N/A' : playerOdds[7]}</button>
     </div>
   )
 }
